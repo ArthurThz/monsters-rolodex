084fe43b1e276fe18d6e73a273916a41010d71e2
Arthur Theodoro | Front End Developer <arthur.theodoro.sva@hotmail.com>
2023-01-27 12:21:40 -0300

    uptades and optimization

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import SearchBox from './components/search-box/search-box';
 function App() {
   const [SearchValue, setSearchValue] = useState('');
   const [Monsters, setMonsters] = useState([]);
+  const [FilteredMonsters, setFilterdMonsters] = useState(Monsters);
+
+  useEffect(() => {
+    let currentFilteredMonsters = Monsters.filter((item) => {
+      return item.name.toLowerCase().includes(SearchValue)
+    })
+    setFilterdMonsters(currentFilteredMonsters)
+
+  }, [Monsters, SearchValue]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -14,21 +23,17 @@ function App() {
       .catch((err) => console.log(err))
   }, [])
 
-  let filteredMonsters = Monsters.filter((item) => {
-    return item.name.toLowerCase().includes(SearchValue)
-  })
-
-  let eventHandler = (e) => {
+  let SearchBoxEventHandler = (e) => {
     setSearchValue(e.target.value.toLowerCase())
   }
 
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      <SearchBox onChangeHandler={eventHandler}
+      <SearchBox onChangeHandler={SearchBoxEventHandler}
         placeholder='Search Mosters'
         className='monsters-search-box' />
-      <CardsList monsters={filteredMonsters} />
+      <CardsList monsters={FilteredMonsters} />
     </div>
   )
 }
